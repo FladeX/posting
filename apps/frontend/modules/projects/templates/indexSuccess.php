8bc0c8e2163e06bd38ba600c7c25a5a7a01580d0
Max Istlyaev <FladeX@yandex.ru>
2012-01-04 16:55:18 +0300

    выделил список проектов в отдельный шаблон

--- apps/frontend/modules/projects/templates/indexSuccess.php
+++ apps/frontend/modules/projects/templates/indexSuccess.php
@@ -115,39 +115,7 @@
 		</ul>
 	</div>
 
-	<table id="tasksTable" class="dataTable">
-	<thead>
-	<tr>
-		<th>
-			<a href="#" class="sortDesc">дата добавления</a>
-		</th>
-		<th>
-			<a href="#">цена</a>
-		</th>
-		<th class="last">
-			<a href="#">претенденты</a>
-		</th>
-	</tr>
-	</thead>
-	<tbody>
-
-	<?php foreach ($projects as $i => $project): ?>
-	<tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
-		<td>
-			<span class="date">[<?php echo $project->getDateTimeObject('created_at')->format('d.m.Y/h:m') ?>]</span>
-			<span class="title">
-				<a href="<?php echo url_for('projects/show?id='.$project->getId()) ?>"><?php echo $project->getTitle() ?></a>
-			</span>
-			<span class="description">
-				<?php echo $project->getText() ?>
-			</span>
-		</td>
-		<td class="value"><?php echo $project->getPrice() ?></td>
-		<td class="value last">2</td>
-	</tr>
-	<?php endforeach; ?>
-	</tbody>
-	</table>
+	<?php include_partial('projects/list', array('projects' => $projects)) ?>
 
 	<div id="paging">
 		<div class="numbers">
